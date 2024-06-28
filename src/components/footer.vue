35484c31ce2b46b2373c2447c8dddd9378b5e585
Rorical <46294886+Rorical@users.noreply.github.com>
2024-06-28 02:36:02 +0100

    modify footer

---
+++ src/components/footer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+</script>
+
+<template>
+  <footer class="text-center text-sm mt-6 mb-5" v-if="route.name !== 'Chat'">
+    <p class="font-bold">&copy;Replica by <a href="https://psykura.one" target="_blank">Psykura Lab</a></p>
+    <p>目前有的问题请来QQ群聊：961342365</p>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
